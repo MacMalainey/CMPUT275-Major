c24034694e43efbb7611474d0d2b52456583d9b4
loravocado <lora@ualberta.ca>
2020-03-28 18:03:21 -0600

    yeet

--- map.h
+++ map.h
@@ -14,19 +14,18 @@ class Junction {
   // But this is easier
   friend class Map;
 
- private:
-  uint16_t x;
-  uint16_t y;
+  private:
+    uint16_t x;
+    uint16_t y;
 
-  Junction** adjacent;
-  uint8_t id;  // Used for hashing default set to 255 to mark invalid ID
+    Junction** adjacent;
+    uint8_t id;  // Used for hashing default set to 255 to mark invalid ID
 
-    public:
-
+  public:
     Junction(uint16_t x, uint16_t y);
 
-  Junction* next(Orientation d);
-  Orientation link(Junction* j);
+    Junction* next(Orientation d);
+    Orientation link(Junction* j);
 };
 
 class Map {
