4821d773d9ac351e0128ba8aeee3668f0d014bd4
Michael Kwok <michael.kwok00@gmail.com>
2020-04-07 20:53:53 -0600

    One indexed junctions

--- include/map.h
+++ include/map.h
@@ -46,21 +46,20 @@ struct MapBuilder {
   uint16_t linkCount = 0;
 
   void SetJunctionCount(uint8_t count) {
+    junctionCount = count;
     copy_arr = new Junction *[count];
     links = new Point[count * 4];
   }
 
-  void SetJunctionLocations(uint16_t x, uint16_t y) {
+  void SetJunctionLocations(uint8_t id, uint16_t x, uint16_t y) {
     auto newJunction = new Junction(x, y);
-    newJunction->id = junctionCount;
-    copy_arr[junctionCount] = newJunction;
+    newJunction->id = id;
+    copy_arr[id - 1] = newJunction;
     Serial.print("Added new junction at ");
-    Serial.print(copy_arr[junctionCount]->x);
+    Serial.print(copy_arr[id - 1]->x);
     Serial.print(" ");
-    Serial.println(copy_arr[junctionCount]->y);
-    Serial.println(copy_arr[junctionCount]->id);
-
-    junctionCount++;
+    Serial.println(copy_arr[id - 1]->y);
+    Serial.println(copy_arr[id - 1]->id);
   }
 
   void LinkJunctions(uint8_t junctionID1, uint8_t junctionID2) {
@@ -69,38 +68,44 @@ struct MapBuilder {
 
   void TestGen() {
     SetJunctionCount(9);
-    SetJunctionLocations(20, 120);
-    SetJunctionLocations(50, 120);
-    SetJunctionLocations(50, 300);
-    SetJunctionLocations(50, 60);
-    SetJunctionLocations(120, 120);
-    SetJunctionLocations(120, 300);
-    SetJunctionLocations(120, 60);
-    SetJunctionLocations(240, 60);
-    SetJunctionLocations(240, 300);
-    LinkJunctions(1u, 2u);
-    LinkJunctions(2u, 3u);
-    LinkJunctions(2u, 4u);
-    LinkJunctions(2u, 5u);
-    LinkJunctions(5u, 6u);
-    LinkJunctions(5u, 7u);
-    LinkJunctions(6u, 3u);
-    LinkJunctions(6u, 9u);
-    LinkJunctions(7u, 4u);
-    LinkJunctions(7u, 8u);
-    LinkJunctions(8u, 9u);
+    LinkJunctions(1, 2);
+    LinkJunctions(2, 3);
+    LinkJunctions(2, 4);
+    LinkJunctions(2, 5);
+    LinkJunctions(5, 6);
+    LinkJunctions(5, 7);
+    LinkJunctions(6, 3);
+    LinkJunctions(6, 9);
+    LinkJunctions(7, 4);
+    LinkJunctions(7, 8);
+    LinkJunctions(8, 9);
+    SetJunctionLocations(1, 20, 120);
+    SetJunctionLocations(2, 50, 120);
+    SetJunctionLocations(3, 50, 300);
+    SetJunctionLocations(4, 50, 60);
+    SetJunctionLocations(5, 120, 120);
+    SetJunctionLocations(6, 120, 300);
+    SetJunctionLocations(7, 120, 60);
+    SetJunctionLocations(8, 240, 60);
+    SetJunctionLocations(9, 240, 300);
   }
 
   Map *Build() {
-    for (auto i = 0; i < linkCount; i++) {
+    for (uint16_t i = 0; i < linkCount; i++) {
       auto currentLink = links[i];
-      copy_arr[currentLink.x]->link(copy_arr[currentLink.y]);
-      Serial.print("Linking: ");
-      Serial.print(currentLink.x);
-      Serial.print(" To ");
-      Serial.println(currentLink.y);
-    }
+      copy_arr[currentLink.x - 1]->link(copy_arr[currentLink.y - 1]);
 
+      auto currentJunction = copy_arr[currentLink.x - 1];
+
+      for (uint8_t i = 0; i < N_ORIENT; i++) {
+        if (currentJunction->adjacent[i] != nullptr) {
+          Serial.print("Linked: ");
+          Serial.print(currentJunction->id);
+          Serial.print(" To ");
+          Serial.println(currentJunction->adjacent[i]->id);
+        }
+      }
+    }
     return new Map(copy_arr, junctionCount);
   }
 };
